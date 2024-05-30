261dac4070572875559a00b1aedbe02d77999dad
hamidmousavi <hamidmousavi0@gmail.com>
2024-05-30 13:37:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "rrelu",
-    version = "0.0.1",
+    version = "0.0.5",
     author = "Seyedhamidreza Mousavi",
     author_email = "seyedhamidreza.mousavi@mdu.se",
     description = "generate reliable relu activation function for DNNs",

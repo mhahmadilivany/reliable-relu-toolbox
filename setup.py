8e1afbbe2f55afa1734043611ad495fa7cee4c52
hamidmousavi0 <hamidmousavi0@gmail.com>
2024-05-30 11:44:46 +0200

    change the name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "rrt",
+    name = "rrelu",
     version = "0.0.1",
     author = "Seyedhamidreza Mousavi",
     author_email = "seyedhamidreza.mousavi@mdu.se",
